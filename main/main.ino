2e6b53022615b34e889bafabdf555e22df89ffc1
Bread <77288737+lucasammer@users.noreply.github.com>
2024-02-10 15:55:54 +0100

    Added conection code

--- main/main.ino
+++ main/main.ino
@@ -6,6 +6,8 @@
 // The pins it is connected to on the arduino
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+int status = WL_IDLE_STATUS;
+
 void setup() {
   Serial.begin(9600);
   lcd.begin(16, 2);
@@ -28,6 +30,51 @@ void setup() {
     lcd.print("WiFi module!");
     while(true);
   }
+
+  String fv = WiFi.firmwareVersion();
+  bool secondLineOccupied = false;
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  {
+    Serial.println("Please update your firmware!");
+    lcd.setCursor(0,1);
+    lcd.print("fw update avail");
+    secondLineOccupied = true;
+  }
+
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+
+    status = WiFi.begin(SSID, PASS);
+    if(secondLineOccupied)
+    {
+      lcd.setCursor(0,0);
+    } else
+    {
+      lcd.setCursor(0,1);
+    }
+
+    int dots = 8;
+    for (int i = 0; i < dots; ++i) 
+    {
+      if(secondLineOccupied)
+      {
+        lcd.setCursor(0,0);
+      } else
+      {
+        lcd.setCursor(0,1);
+      }
+      lcd.print("Connecting");
+      for(int j = 0; j < i; ++j){
+        lcd.print(".");
+        delay(10000/dots);
+      }
+    }
+  }
+
+  lcd.setCursor(0,1);
+  lcd.print("Connected!");
 }
 
 void loop() {
